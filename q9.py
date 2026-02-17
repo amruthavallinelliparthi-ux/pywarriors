28457a5c720ce93f3146348b043a41de03d51827
amruthavallinelliparthi-ux <amruthavallinelliparthi@gmail.com>
2026-02-17 20:31:28 +0530

    q9.py

---
+++ q9.py
@@ -0,0 +1,10 @@
+#team name -pywarriors
+def generate_threes(start: int, end: int) -> list[int]:
+    """
+    Generate a list of numbers from start to end, skipping by 3.
+    """
+
+    if start >= end:
+        return []
+
+    return list(range(start, end, 3))
